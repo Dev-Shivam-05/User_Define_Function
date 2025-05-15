b327c3c08ed24b52e6932b9be1d3d23516ea3205
Shivam Bhadoriya <shivambhadoriya1605@gmail.com>
2025-05-15 21:41:08 +0530

    Second Commit

--- Self_Work/TNRS/program-1.cpp
+++ Self_Work/TNRS/program-1.cpp
@@ -1,10 +1,23 @@
-// TNRS
-
-#include<iostream>
-using namespace std;
+#include<ioistream>
+usin namespace std;
 
 int main()
 {
+    int size;
 
+    cout << "Enter The Size Of Array : ";
+    cin >> size;
+
+    int array[size];
+    
+    int i,max=array[0];
+    for (i=1; i<=size; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    cout << "The Largest Number in the Array is : "<<max<<". ";
     return 0;
 }
